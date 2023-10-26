a7a995aa665333e1c9b66d23d3dc0a56181667eb
Colin Tolliver <colintolliver@gmail.com>
2023-10-26 10:32:47 -0700

    Added a pokedex to the second page

---
+++ main2.js
@@ -0,0 +1,73 @@
+const inputValue = document.getElementById('search');
+const pokeBtn = document.getElementById('btn');
+const pokeContainer = document.getElementById('poke_container');
+let pokemonQueue = [];
+
+pokeBtn.addEventListener('click', function() {
+  const chosenPoke = inputValue.value;
+  getPokemon(chosenPoke);
+});
+
+function allPokemon() {
+  for (let i = 1; i <= 1010; i++) {
+    pokemonQueue.push(i);
+  }
+  processNextPokemon();
+}
+
+allPokemon();
+
+function getPokemon(chosenPoke) {
+  $.get(`https://pokeapi.co/api/v2/pokemon/${chosenPoke}`, function(data) {
+    const pokemonData = data;
+    buildPokeEntry(pokemonData);
+    processNextPokemon();
+  });
+}
+
+function processNextPokemon() {
+  if (pokemonQueue.length > 0) {
+    const nextPokemon = pokemonQueue.shift();
+    $.get(`https://pokeapi.co/api/v2/pokemon/${nextPokemon}`, function(data) {
+      const pokemonData = data;
+      buildPokeEntry(pokemonData);
+      processNextPokemon();
+    });
+  }
+}
+
+function buildPokeEntry(obj) {
+  const html = `
+    <div class="pokemon">
+      <img id="img" src="${obj.sprites.front_default}" alt="${obj.name}">
+      <p>${obj.name.charAt(0).toUpperCase() + obj.name.slice(1)}</p>
+    `;
+  pokeContainer.insertAdjacentHTML('beforeend', html);
+
+  const pokemonEntry = pokeContainer.lastElementChild;
+  pokemonEntry.addEventListener('click', function() {
+    openPopup(obj);
+  });
+}
+
+const popupContainer = document.getElementById('popupContainer');
+const closePopup = document.getElementById('closePopup');
+const popupContent = document.getElementById('popupContent');
+
+function openPopup(pokemonData) {
+  const html = `
+    <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+    <p>Name: ${pokemonData.name}</p>
+    <p>Weight: ${pokemonData.weight} lb.</p>
+    <p>Height: ${pokemonData.height} in"</p>
+  `;
+
+  popupContent.innerHTML = html;
+  popupContainer.style.display = 'block';
+}
+
+function closePopupContainer() {
+  popupContainer.style.display = 'none';
+}
+
+closePopup.addEventListener('click', closePopupContainer);
