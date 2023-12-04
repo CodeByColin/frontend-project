a2d4113d9594ae7f0a9f73e1dff33dbd950819c4
Colin Tolliver <colintolliver@gmail.com>
2023-12-04 09:13:36 -0800

    Pokedex

--- main.js
+++ main.js
@@ -1,115 +1,182 @@
-// Select elements
-const shiba = document.getElementById('shibabtn');
-const shibaInput = document.getElementById('shibaSearch');
-const bird = document.getElementById('birdbtn');
-const birdInput = document.getElementById('birdSearch');
-const cat = document.getElementById('catbtn');
-const catInput = document.getElementById('catSearch');
-const imageContainer = document.getElementById('image-container');
-const galleryImages = document.getElementById('gallery-images');
-const toggle = document.getElementById('toggle')
-const gallery = document.getElementById('gallery')
+// Get DOM elements
+const inputValue = document.getElementById('search');
+const pokeBtn = document.getElementById('btn');
+const pokeContainer = document.getElementById('poke_container');
+let pokemonQueue = [];
 
-let inputValue
-let imgArr;
+const pokeVideoContainer = document.getElementById('pokeVideoContainer');
+const closePokeVideo = document.getElementById('closePokeVideo');
 
-// Event listeners for animal buttons
-shiba.addEventListener('click', function () {
-  inputValue = shibaInput.value;
-  fetchDataAndBuildImages('shibes');
+// Function to open the video container
+function openPokeVideo() {
+  pokeVideoContainer.style.display = 'block';
+}
+
+// Function to close the video container
+function closePokeVideoContainer() {
+  pokeVideoContainer.style.display = 'none';
+}
+
+// Add a "load" event listener to open the video container when the page loads
+window.addEventListener('load', openPokeVideo);
+
+// Add a "click" event listener to close the video container
+closePokeVideo.addEventListener('click', closePokeVideoContainer);
+
+// Add a "click" event listener to the "Choose that Pokemon" button
+pokeBtn.addEventListener('click', function() {
+  const chosenPoke = inputValue.value;
+  getPokemon(chosenPoke);
 });
 
-bird.addEventListener('click', function () {
-  inputValue = birdInput.value;
-  fetchDataAndBuildImages('birds');
-});
+// Function to populate the queue with Pokemon IDs
+function allPokemon() {
+  for (let i = 1; i <= 1010; i++) {
+    pokemonQueue.push(i);
+  }
+  processNextPokemon();
+}
 
-cat.addEventListener('click', function () {
-  inputValue = catInput.value;
-  fetchDataAndBuildImages('cats');
-});
+// Initialize the process by populating the queue with Pokemon IDs
+allPokemon();
 
-// Fetch data and build images
-function fetchDataAndBuildImages(animalType) {
-  $.get(`http://shibe.online/api/${animalType}?count=${inputValue}&urls=true&httpsUrls=true`, function (data) {
-    imgArr = data;
-    buildImages(imgArr);
+// Function to clear the displayed Pokemon
+function clearPokemon() {
+  pokeContainer.innerHTML = '';
+}
+
+// Add "click" event listeners for region buttons
+kantoBtn.addEventListener('click', () => displayPokemonByRegion('kanto'));
+johtoBtn.addEventListener('click', () => displayPokemonByRegion('johto'));
+hoennBtn.addEventListener('click', () => displayPokemonByRegion('hoenn'));
+sinnohBtn.addEventListener('click', () => displayPokemonByRegion('sinnoh'));
+unovaBtn.addEventListener('click', () => displayPokemonByRegion('unova'));
+kalosBtn.addEventListener('click', () => displayPokemonByRegion('kalos'));
+alolaBtn.addEventListener('click', () => displayPokemonByRegion('alola'));
+galarBtn.addEventListener('click', () => displayPokemonByRegion('galar'));
+paldeaBtn.addEventListener('click', () => displayPokemonByRegion('paldea'));
+allBtn.addEventListener('click', function() {
+    clearPokemon();
+    allPokemon();
+})
+
+// Function to display Pokemon from a specific region
+function displayPokemonByRegion(region) {
+  clearPokemon();
+
+  let startId, endId;
+
+  // Determine the range of Pokemon IDs based on the selected region
+  switch (region) {
+    case 'kanto':
+      startId = 1;
+      endId = 151;
+      break;
+    case 'johto':
+      startId = 152;
+      endId = 251;
+      break;
+    case 'hoenn':
+      startId = 252;
+      endId = 386;
+      break;
+    case 'sinnoh':
+      startId = 387;
+      endId = 493;
+      break;
+    case 'unova':
+      startId = 494;
+      endId = 649;
+      break;
+    case 'kalos':
+      startId = 650;
+      endId = 721;
+      break;
+    case 'alola':
+      startId = 722;
+      endId = 809;
+      break;
+    case 'galar':
+      startId = 810;
+      endId = 898;
+      break;
+    case 'paldea':
+      startId = 899;
+      endId = 1010;
+      break;
+    default:
+      return;
+  }
+
+  // Add Pokemon IDs to the queue within the specified range
+  for (let i = startId; i <= endId; i++) {
+    pokemonQueue.push(i);
+  }
+
+  // Start processing the next Pokemon
+  processNextPokemon();
+}
+
+// Function to fetch Pokemon data by ID
+function getPokemon(chosenPoke) {
+  $.get(`https://pokeapi.co/api/v2/pokemon/${chosenPoke}`, function(data) {
+    const pokemonData = data;
+    clearPokemon();
+    buildPokeEntry(pokemonData);
+    processNextPokemon();
   });
 }
 
-// Build and display images
-function buildImages(imgArr) {
-  // Remove excess images if the total exceeds 1000
-  const currentImages = imageContainer.getElementsByTagName('img');
-  if (currentImages.length + imgArr.length > 1000) {
-    const imagesToRemove = currentImages.length + imgArr.length - 1000;
-    for (let i = 0; i < imagesToRemove; i++) {
-      if (imageContainer.lastChild) {
-        imageContainer.removeChild(imageContainer.lastChild);
-      }
-    }
+// Function to process the next Pokemon in the queue
+function processNextPokemon() {
+  if (pokemonQueue.length > 0) {
+    const nextPokemon = pokemonQueue.shift();
+    $.get(`https://pokeapi.co/api/v2/pokemon/${nextPokemon}`, function(data) {
+      const pokemonData = data;
+      buildPokeEntry(pokemonData);
+      processNextPokemon();
+    });
   }
-
-  // Add new images to the container at the top
-  imgArr.forEach(function (imgUrl) {
-    const imgElement = document.createElement('img');
-    imgElement.src = imgUrl;
-    if (imageContainer.firstChild) {
-      imageContainer.insertBefore(imgElement, imageContainer.firstChild);
-    } else {
-      imageContainer.appendChild(imgElement);
-    }
-  });
-
-  // Update local storage with the current gallery images
-  saveGalleryToLocalStorage();
 }
 
-// Load saved gallery images from local storage on page load
-loadGalleryFromLocalStorage();
+// Function to build and display a Pokemon entry
+function buildPokeEntry(obj) {
+  const html = `
+    <div class="pokemon">
+      <img id="img" src="${obj.sprites.front_default}" alt="${obj.name}">
+    `;
+  pokeContainer.insertAdjacentHTML('beforeend', html);
 
-// Event listener for adding images to the gallery
-imageContainer.addEventListener('click', function (e) {
-  if (e.target && e.target.nodeName === 'IMG') {
-    const imgClone = e.target.cloneNode(true);
-    imgClone.onclick = removeImageFromGallery;
-    galleryImages.appendChild(imgClone);
-
-    // Update local storage when adding an image to the gallery
-    saveGalleryToLocalStorage();
-  }
-});
-
-// Remove an image from the gallery
-function removeImageFromGallery() {
-  galleryImages.removeChild(this);
-
-  // Update local storage when removing an image from the gallery
-  saveGalleryToLocalStorage();
-}
-
-// Save the gallery images to local storage
-function saveGalleryToLocalStorage() {
-  const galleryImagesData = [];
-  const galleryImageElements = galleryImages.getElementsByTagName('img');
-  for (const img of galleryImageElements) {
-    galleryImagesData.push({ src: img.src });
-  }
-  localStorage.setItem('galleryImages', JSON.stringify(galleryImagesData));
-}
-
-// Load saved gallery images from local storage
-function loadGalleryFromLocalStorage() {
-  const savedGalleryImages = JSON.parse(localStorage.getItem('galleryImages')) || [];
-  savedGalleryImages.forEach(function (imageData) {
-    const imgElement = document.createElement('img');
-    imgElement.src = imageData.src;
-    imgElement.onclick = removeImageFromGallery;
-    galleryImages.appendChild(imgElement);
+  const pokemonEntry = pokeContainer.lastElementChild;
+  pokemonEntry.addEventListener('click', function() {
+    openPopup(obj);
   });
 }
 
+// Get DOM elements for the popup
+const popupContainer = document.getElementById('popupContainer');
+const closePopup = document.getElementById('closePopup');
+const popupContent = document.getElementById('popupContent');
 
+// Function to open the popup with Pokemon details
+function openPopup(pokemonData) {
+    console.log(pokemonData)
+  const html = `
+    <img id="bigPoke" src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+    <p id="text">${pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}</p>
+    <p id="text">Weight: ${pokemonData.weight} lb.</p>
+    <p id="text">Height: ${pokemonData.height} in"</p>
+    <p id="text">Type: ${pokemonData.types[0].type.name.charAt(0).toUpperCase() + pokemonData.types[0].type.name.slice(1)}</p>
+  `;
 
+  popupContent.innerHTML = html;
+  popupContainer.style.display = 'block';
+}
 
+// Function to close the popup
+function closePopupContainer() {
+  popupContainer.style.display = 'none';
+}
 
+// Add a "click" event listener to close the popup
+closePopup.addEventListener('click', closePopupContainer);
